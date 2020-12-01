7b3e1762235716c6ef0c4a32e8e0085a9e352bdc
Dragan Pepic <d.pepic@hotmail.com>
2020-12-01 21:03:45 +0100

    bolji primer

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp20
 {
@@ -6,191 +8,99 @@ namespace ConsoleApp20
 	{
 		static void Main(string[] args)
 		{
-			var p = new Proizvodnja(Kreditne.Srebro, 400, 3000);
-			IKK neka = p.Daaaj();
+			
 		}
 	}
 
-	public enum Kreditne
-	{ 
-		Bronza,
-		Srebro,
-		Zlato
-	}
-
-	public class Proizvodnja
+	public abstract class Proracun
 	{
-		private IFabrikaKreditnih _fab;
+		public string _rezultat;
+		public ICollection<int> Brojevi;
+		public abstract void Racunaj();
+	}
 
-		public Proizvodnja(Kreditne koja, int l, int pp)
+	public class Zbir : Proracun
+	{
+		public Zbir(ICollection<int> br)
 		{
-			switch(koja)
-			{
-				case Kreditne.Bronza:
-					_fab = new BronzanaFabrika(l, pp);
-					break;
-				case Kreditne.Srebro:
-					_fab = new NekaTamoFab(l, pp);
-					break;
-				case Kreditne.Zlato:
-					_fab = new ZlatnaFabrika(l, pp);
-					break;
-			}
+			Brojevi = br;
 		}
-		public IKK Daaaj() =>
-			_fab.DajKreditnu();
-	}
-
-	public interface IKK
-	{
-		public int Limit { get; set; }
-		public int ProvizijaProc { get; set; }
-		public string Naziv { get; set; }
-	}
-
-	class NekaTamo : IKK
-	{
-		private int _limit;
-		private int _provizijaProc;
-		private string _naziv;
-
-		public int Limit { get => _limit; set => _limit = value; }
-		public int ProvizijaProc { get => _provizijaProc; set => _provizijaProc = value; }
-		public string Naziv { get => _naziv; set => _naziv = value; }
-
-		public NekaTamo(int l, int pp)
+		public override void Racunaj()
 		{
-			Naziv = "Bronzana";
-			Limit = l;
-			ProvizijaProc = pp;
+			int zbir = Brojevi.ToList().Aggregate((svi, x) => svi += x);
+			_rezultat = $"Zbir je {zbir}";
 		}
 	}
 
-	class Bronzana : IKK
+	public class MMM : Proracun
 	{
-		private int _limit;
-		private int _provizijaProc;
-		private string _naziv;
-
-		public int Limit { get => _limit; set => _limit = value; }
-		public int ProvizijaProc { get => _provizijaProc; set => _provizijaProc = value; }
-		public string Naziv { get => _naziv; set => _naziv = value; }
-
-		public Bronzana(int l, int pp)
+		public MMM(ICollection<int> br)
 		{
-			Naziv = "Bronzana";
-			Limit = l;
-			ProvizijaProc = pp;
+			Brojevi = br;
+		}
+
+		public override void Racunaj()
+		{
+			double mean = Brojevi.ToList().Average();
+			double median = 0;
+			if (Brojevi.Count % 2 == 1)
+				median = Brojevi.ToList()[Brojevi.Count / 2];
+			else
+				median = (Brojevi.ToList()[Brojevi.Count / 2] + Brojevi.ToList()[(Brojevi.Count / 2) + 1]) / 2;
+			double mode = Brojevi.ToList().GroupBy(b => b).OrderByDescending(b => b.Count()).Select(b => b.Key).First();
+			_rezultat = $"Mean: {mean}, Mode:{mode}, Median:{median}";
 		}
 	}
 
-	class Srebrna : IKK
+	public abstract class Izvestaj
 	{
-		private int _limit;
-		private int _provizijaProc;
-		private string _naziv;
+		protected ICollection<Proracun> Proracuni;
 
-		public int Limit { get => _limit; set => _limit = value; }
-		public int ProvizijaProc { get => _provizijaProc; set => _provizijaProc = value; }
-		public string Naziv { get => _naziv; set => _naziv = value; }
-
-		public Srebrna(int l, int pp)
+		public Izvestaj()
 		{
-			Naziv = "Srebrna";
-			Limit = l;
-			ProvizijaProc = pp;
+			NapraviIzvestaj();
+		}
+
+		protected abstract void NapraviIzvestaj();
+	}
+
+	public class A : Izvestaj
+	{
+		ICollection<int> brr;
+		public A(ICollection<int> b)
+		{
+			brr = b;
+		}
+		protected override void NapraviIzvestaj()
+		{
+			Proracuni.Add(new MMM(brr));
 		}
 	}
 
-	class Zlatna : IKK
+	public class B : Izvestaj
 	{
-		private int _limit;
-		private int _provizijaProc;
-		private string _naziv;
-
-		public int Limit { get => _limit; set => _limit = value; }
-		public int ProvizijaProc { get => _provizijaProc; set => _provizijaProc = value; }
-		public string Naziv { get => _naziv; set => _naziv = value; }
-
-		public Zlatna(int l, int pp)
+		ICollection<int> brr;
+		public B(ICollection<int> b)
 		{
-			Naziv = "Zlatna";
-			Limit = l;
-			ProvizijaProc = pp;
+			brr = b;
+		}
+		protected override void NapraviIzvestaj()
+		{
+			Proracuni.Add(new Zbir(brr));
 		}
 	}
 
-
-	interface IFabrikaKreditnih
+	public class C : Izvestaj
 	{
-		public IKK DajKreditnu();
-	}
-
-	class BronzanaFabrika : IFabrikaKreditnih
-	{
-		private int _limit;
-		private int _pp;
-
-		public BronzanaFabrika(int l, int pp)
+		ICollection<int> brr;
+		public C(ICollection<int> b)
 		{
-			_limit = l;
-			_pp = pp;
+			brr = b;
 		}
-
-		public IKK DajKreditnu()
+		protected override void NapraviIzvestaj()
 		{
-			return new Bronzana(_limit, _pp);
-		}
-	}
-
-	class SrebrnaFabrika : IFabrikaKreditnih
-	{
-		private int _limit;
-		private int _pp;
-
-		public SrebrnaFabrika(int l, int pp)
-		{
-			_limit = l;
-			_pp = pp;
-		}
-
-		public IKK DajKreditnu()
-		{
-			return new Srebrna(_limit, _pp);
-		}
-	}
-
-	class NekaTamoFab : IFabrikaKreditnih
-	{
-		private int _limit;
-		private int _pp;
-
-		public NekaTamoFab(int l, int pp)
-		{
-			_limit = l;
-			_pp = pp;
-		}
-
-		public IKK DajKreditnu()
-		{
-			return new NekaTamo(_limit, _pp);
-		}
-	}
-
-	class ZlatnaFabrika : IFabrikaKreditnih
-	{
-		private int _limit;
-		private int _pp;
-
-		public ZlatnaFabrika(int l, int pp)
-		{
-			_limit = l;
-			_pp = pp;
-		}
-
-		public IKK DajKreditnu()
-		{
-			return new Zlatna(_limit, _pp);
+			Proracuni.Add(new Zbir(brr));
+			Proracuni.Add(new MMM(brr));
 		}
 	}
 }
